abf6d58e1cc1dc39bef5f015f5000b8ad3d804da
Csaba Király <kiraly.csaba17@gmail.com>
2025-11-11 18:21:45 +0100

    Categories index, Questions index és store function

--- src/app/Http/Controllers/CategoriesController.php
+++ src/app/Http/Controllers/CategoriesController.php
@@ -11,7 +11,8 @@ class CategoriesController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
     }
 
     /**
