c4971f33c14d52ced9428b6c4e1b607a7498b6de
Király Gábor <kiraly.gabor11@gmail.com>
2025-11-12 21:45:28 +0100

    Frontend: index.blade.php frissítve, két gomb a főoldalon

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -3,4 +3,9 @@
 @section('content')
 <h1>Főoldal</h1>
 
+<div>
+    <a href="{{ route('votes') }}">Szavazok</a>
+    <a href="{{ route('questions.create') }}" >Új szavazást indítok</a>
+</div>
+
 @endsection
