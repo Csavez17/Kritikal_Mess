34beff0eed0e5e9aee745e957ad2eaa7f2fdadc8
Király Gábor <kiraly.gabor11@gmail.com>
2025-11-12 20:52:36 +0100

    votes.blade.php feltöltve: kérdések listája

--- src/resources/views/questions/votes.blade.php
+++ src/resources/views/questions/votes.blade.php
@@ -1,6 +1,24 @@
 @extends('layout')
 
 @section('content')
-<h1>Kérdések:</h1>
+<h1>Kérdések amikre szavazni lehet:</h1>
 
+@if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if($questions->isEmpty())
+    <p>Nincsenek kérdések.</p>
+@else
+    <ul>
+        @foreach($questions as $question)
+            <li>
+                <strong>{{ $question->id }} - {{ $question->question_name }}</strong>
+            </li>
+            
+        @endforeach
+    </ul>
+@endif
 @endsection
