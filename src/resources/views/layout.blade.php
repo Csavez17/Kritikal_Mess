dfc256c4efe5e223dc9217b7017f075dc1969c81
Király Gábor <kiraly.gabor11@gmail.com>
2025-11-22 20:50:56 +0100

    Feat: Finalize create form structure and layout integration

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -4,26 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KritikalMess</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
 </head>
 <body>
     <header>
-        <h1>KritikalMess</h1>
+        <h1></h1>
         <nav>
             <ul>
-                <li><a href="{{route('index')}}">Főoldal</a></li>
-                <li><a href="{{route('questions.index')}}">Kérdések</a></li>
-                <li><a href="{{route('questions.create')}}">Új szavazás</a></li>
-                <li><a href="{{route('sign.register')}}">Regisztráció</a></li>
-                <li><a href="{{route('sign.login')}}">Bejelentkezés</a></li>
+                <li><a href="{{ route('index') }}">Főoldal</a></li>
+                <li><a href="{{ route('votes') }}">Kérdések</a></li>
+                <li><a href="{{ route('questions.create') }}">Új szavazás</a></li>
+                <li><a href="{{ route('sign.login') }}">Bejelentkezés</a></li>
+                <li><a href="{{ route('sign.register') }}">Regisztráció</a></li>
             </ul>
         </nav>
     </header>
-    <main>
-        @yield('content')  
+
+   <main class="image-wrapper" 
+      style="background-image: url('{{ asset('Kritikal_Mess_kep.jpg') }}');">
+        <div class="content-on-image">
+            @yield('content')
+        </div>
     </main>
+
     <footer>
         <p>&copy; 2025 KritikalMess</p>
     </footer>
-        
 </body>
 </html>
--- src/resources/views/questions/create.blade.php
+++ src/resources/views/questions/create.blade.php
@@ -4,13 +4,25 @@
 <h1>Új szavazást indítok</h1>
 
 @error('question_name')
-<div class="alert alert-warning">{{ $message }}</div>
+    <div class="alert alert-warning">{{ $message }}</div>
 @enderror
 
 <form action="{{ route('questions.store') }}" method="post">
     @csrf
-    <label class= "form-label" for="question_name">A kérdésem:</label>
-    <textarea class= "form-textarea" type="text" id="question_name" name="question_name"></textarea>
-    <button class= "button" type="submit">Ment</button>
+    
+    <label class="form-label" for="question_name">A kérdésem:</label>
+    <textarea class="form-textarea" id="question_name" name="question_name"></textarea>
+
+    <label class="form-label" for="category_id">Kategória</label>
+    <select class="category" name="category_id" id="category_id">
+        
+        <option value="" disabled selected>Válassz kategóriát...</option>
+
+        @foreach($categories as $category)
+            <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+        @endforeach
+
+    </select>
+    <button class="button" type="submit">Mentés</button>
 </form>
 @endsection
