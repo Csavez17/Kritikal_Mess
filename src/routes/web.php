7c1122287d10473f6619fdb89ced003b30167c61
Csaba Király <kiraly.csaba17@gmail.com>
2025-12-02 17:23:55 +0100

    AdminDashboard-, Login-, ProfileController / UserPolicy

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,15 +1,30 @@
 <?php
 
+use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuestionsController;
+use App\Http\Controllers\VotesController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/questions/{question}/vote', [VotesController::class, 'store'])->name('vote');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', [LoginController::class, 'show']);
+
+Route::get('/index', function () {
+    return view('index');
+})->name('index');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/admin', [AdminDashboardController::class, 'show'])->middleware('can:admin-access');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -18,3 +33,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('questions', QuestionsController::class);
+Route::resource('categories', CategoriesController::class);
